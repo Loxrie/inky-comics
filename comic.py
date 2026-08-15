15faf957c95d829586ef46224fe8989d43895f8c
Paul Downs <diziet@thelostsouls.org.uk>
2026-08-15 18:09:40 +0100

    Initial commit, main features done

---
+++ comic.py
@@ -0,0 +1,183 @@
+"""
+Python script to fetch a randomised comic cover from the Comic Vine API and show it on an Inky Impression display.
+You will need to sign up for an API key at https://comicvine.gamespot.com/api/ to use this script.
+Change the search query to the comic series you want to display!
+"""
+
+import random
+from io import BytesIO
+
+import requests
+from PIL import Image
+import os
+
+from slugify import slugify
+
+DEBUG = os.environ.get('DEBUG', False)
+
+if DEBUG is False:
+    from inky.auto import auto
+
+# Settings
+from settings import SEARCH_QUERIES, RANDOM_VOLUME, SAVE_PATH, API_KEY, BASE_URL, HEADERS
+
+# Inky Impression display setup
+if DEBUG is False:
+    inky_display = auto()
+
+def find_volume_id(api_key, query):
+    # Our first API call finds a list of volumes that match the search query, and then picks one
+    params = {"api_key": api_key, "format": "json", "query": query, "resources": "volume", "limit": 10 if RANDOM_VOLUME[query] else 1}
+    response = requests.get(f"{BASE_URL}search/", headers=HEADERS, params=params)
+    response.raise_for_status()
+    data = response.json()
+    response.close()
+    results = data.get("results", [])
+    if results:
+        for idx, volume in enumerate(results, 1):
+            print(f"{idx}: {volume['name']} (ID: {volume['id']}, Start Year: {volume.get('start_year', 'N/A')})")
+
+        if RANDOM_VOLUME[query] is True:
+            # Pick a random volume from the search results
+            chosen = random.choice(results)
+            print(f"Randomly selected: {chosen['name']} (ID: {chosen['id']})")
+        else:
+            # Pick the first volume from the search results
+            chosen = results[0]
+            print("Picked first result!")
+        return chosen["id"]
+    else:
+        raise ValueError(f"No volumes found for {query}.")
+
+def get_character_url(api_key, query):
+    # Our first API call finds a list of volumes that match the search query, and then picks one
+    params = {
+        "api_key": api_key,
+        "format": "json",
+        "filter": f"name:{query}", 
+        "field_list": "name,api_detail_url,count_of_issue_appearances", 
+        "sort": "count_of_issue_appearances:desc",
+        "limit": 100
+    }
+    response = requests.get(f"{BASE_URL}characters/", headers=HEADERS, params=params)
+    response.raise_for_status()
+    data = response.json()
+    response.close()
+    results = data.get("results", [])
+    if results:
+        chosen = results[0]
+        print(f"Chosen {chosen['name']} appeared in {chosen['count_of_issue_appearances']} comics")
+        return chosen["api_detail_url"]
+    else:
+        raise ValueError(f"No character url found for {query}.")
+
+def get_random_character_appearance(api_key, api_detail_url):
+    # Our first API call finds a list of volumes that match the search query, and then picks one
+    params = {"api_key": api_key, "format": "json", "field_list": "name,volume,issue_credits", "limit": 100}
+    response = requests.get(f"{api_detail_url}", headers=HEADERS, params=params)
+    response.raise_for_status()
+    data = response.json()
+    response.close()
+    results = data.get("results", {})
+    if results:
+        chosen = random.choice(results["issue_credits"])
+        print(f"Chose issue {chosen['name']}")
+        return chosen["api_detail_url"]
+    else:
+        raise ValueError(f"No character url found for {query}.")
+
+def find_random_comic_image(api_key, series_id):
+    # Once we know the volume ID we can do a second API call to fetch a list of issues and pick a random cover image
+    params = {"api_key": api_key, "format": "json", "filter": f"volume:{series_id}", "limit": 100}
+    response = requests.get(f"{BASE_URL}issues/", headers=HEADERS, params=params)
+    response.raise_for_status()
+    data = response.json()
+    response.close()
+    results = data.get("results", [])
+    if results:
+        issue = random.choice(results)
+        # print a link to the issue page on Comic Vine
+        print(f"Find out more: {issue['site_detail_url']}")
+        image_link = issue["image"]["original_url"]
+        image_name = slugify(
+            f"{issue['volume']['name']} {issue['name']} {issue['issue_number']}_{issue['id']}",
+            allow_unicode=True,
+            separator=' '
+        )
+        return image_link, image_name
+    else:
+        raise ValueError("No comic issues found for the specified series.")
+
+def get_random_comic_image(api_key, comic_url):
+    # Once we know the volume ID we can do a second API call to fetch a list of issues and pick a random cover image
+    params = {"api_key": api_key, "format": "json"}
+    response = requests.get(f"{comic_url}", headers=HEADERS, params=params)
+    response.raise_for_status()
+    data = response.json()
+    response.close()
+    issue = data.get("results", [])
+    if issue:
+        # print a link to the issue page on Comic Vine
+        print(f"Random issue selected: ID: {issue['id']}")
+        print(f"Find out more: {issue['site_detail_url']}")
+        image_link = issue["image"]["original_url"]
+        image_name = f"{issue['volume']['name']} {issue['name']} {issue['issue_number']}_{issue['id']}"
+        return image_link, image_name
+    else:
+        raise ValueError("No comic issues found for the specified series.")
+
+
+def get_image_from_url(image_url, name):
+    # Display image on Inky Impression
+    response = requests.get(image_url)
+    response.raise_for_status()
+    image = Image.open(BytesIO(response.content))
+    response.close()
+    if SAVE_PATH is not None:
+        image.save(f"{SAVE_PATH}/{name}.png")
+    return image
+
+def display_image_on_inky(image):
+    if DEBUG is False:
+        # Rotate the image if it is taller than it is wide
+        if image.height > image.width:
+            image = image.rotate(-90, expand=True)
+            
+        
+        image = image.resize(inky_display.resolution)
+        inky_display.set_image(image, saturation=0.5)
+        print("Updating Inky Impression!")
+        inky_display.show()
+
+def displayComic():
+    try:
+        # Pick a random search term from the list
+        (search_type, search_query) = random.choice(SEARCH_QUERIES)
+        print(f"Chose type {search_type} query {search_query}")
+        comic_image_url = None
+        image_name = None
+        if search_type == "Volume":
+            volume_id = find_volume_id(API_KEY, search_query)
+            print(f"Found volume id {volume_id}")
+            comic_image_url, image_name  = find_random_comic_image(API_KEY, volume_id)
+        elif search_type == "Character":
+            character_url = get_character_url(API_KEY, search_query)
+            random_comic_url = get_random_character_appearance(API_KEY, character_url)
+            comic_image_url, image_name = get_random_comic_image(API_KEY, random_comic_url)
+        if (comic_image_url is not None):
+            print(f"Would open : {comic_image_url}")
+            image = get_image_from_url(comic_image_url, image_name)
+            if DEBUG is False:
+                display_image_on_inky(image)
+    except Exception as e:
+        print(f"Error: {e}")
+
+def displayPoster(file):
+    try:
+        if DEBUG is False:
+            display_image_on_inky(Image.open(file))
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    displayComic()
