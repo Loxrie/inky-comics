e4e74afbc02650e28a7e71ab9ef165bd6b23cb2e
Paul Downs <diziet@thelostsouls.org.uk>
2026-08-26 01:39:46 +0100

    Add settings, fix a bug, change threshold and improve logging
    Signed-off-by: Paul Downs <diziet@thelostsouls.org.uk>

--- comicvine.py
+++ comicvine.py
@@ -7,10 +7,12 @@ from os import path
 
 from slugify import slugify
 
+
 class ComicCollisionError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
 
 class ComicVine:
     def __init__(
@@ -19,28 +21,33 @@ class ComicVine:
         random,
         base_url="https://comicvine.gamespot.com/api/",
         headers={"User-Agent": "Python Comic Vine Random Image Client"},
+        dedupe=False,
+        history_size=122,
     ):
         self.api_key = api_key
         self.random = random
         self.base_url = base_url
         self.headers = headers
-        self.seen = deque(maxlen=28)
-        self.task_list = []
+
+        self.dedupe = dedupe
         self.call_map = []
-        self.query = None
-        self.last_chance = False
-        self.bummer = None
-        if path.isfile("seen_comics.pickle"):
-            with open('seen_comics.pickle', 'rb') as f:
-                self.seen = pickle.load(f)
-            # logging.debug(f"Restored seen hashes, {self.seen}")
+
+        if self.dedupe is True:
+            self.seen = deque(maxlen=history_size)
+            self.task_list = []
+            self.query = None
+            self.last_chance = False
+            self.bummer = None
+            if path.isfile("seen_comics.pickle"):
+                with open("seen_comics.pickle", "rb") as f:
+                    self.seen = pickle.load(f)
 
     def build_tasklist(self, *args):
-        self.last_chance = False
+        self.reset()
         search_type = args[0]
         self.query = args[1:]
         print(f"Building tasklist {search_type} with query {self.query}")
-        if (search_type == "Volume"):
+        if search_type == "Volume":
             self.task_list.append(self.get_random_volume_url)
             self.task_list.append(self.get_random_comic_url)
         else:
@@ -58,38 +65,46 @@ class ComicVine:
         except ComicCollisionError as ce:
             if self.last_chance is not True:
                 history = self.rewind()
-                self.call_map.clear()
-                self.task_list.clear()
+                self.reset()
                 self.task_list = [e[0] for e in history]
                 self.query = history[0][2]
                 self.last_chance = True
-                logging.debug(f"Rerunning {self.task_list} with initial query {self.query}")
-                self.query = self.run()
-                logging.info("Dedupe comic: Success")
+                logging.debug(
+                    f"Rerunning {self.task_list} with initial query {self.query}"
+                )
             else:
-                # I guess just return the dupe if we done fucked up
-                logging.info("Dedupe comic: Failure - returning original")
-                self.query = self.bummer
+                return self.bummer
+
+        if self.last_chance is True:
+            self.query = self.run()
+
         return self.query
+
+    def reset(self):
+        self.task_list.clear()
+        self.call_map.clear()
+        self.query = None
+        self.last_chance = False
 
     def get_hash(self, item: str) -> str:
         return hashlib.sha256(item.encode()).hexdigest()
 
     def add_hash(self, hash: str):
         self.seen.append(hash)
-        with open('seen_comics.pickle', 'wb') as f:
+        with open("seen_comics.pickle", "wb") as f:
             pickle.dump(self.seen, f, pickle.HIGHEST_PROTOCOL)
 
     def rewind(self) -> str or bool:
         for call in reversed(self.call_map):
             function, choices, query = call
-            if choices > 1:
-                call_index = [i for i, call in enumerate(self.call_map) if call[0] == function][0]
+            if choices > 2:
+                call_index = [
+                    i for i, call in enumerate(self.call_map) if call[0] == function
+                ][0]
                 return self.call_map[call_index:]
 
     def set_random(self, rng):
         self.random = rng
-
 
     # Get 100 volumes (max) matching name and return a random api detail url from it
     def get_random_volume_url(self, *args) -> str:
@@ -130,7 +145,7 @@ class ComicVine:
                 logging.info(
                     f"Picked first result: {chosen['name']} (ID: {chosen['id']}) {chosen['api_detail_url']}"
                 )
-            return chosen["api_detail_url"],
+            return (chosen["api_detail_url"],)
         else:
             raise ValueError(f"No volumes found for {query}.")
 
@@ -161,7 +176,7 @@ class ComicVine:
             logging.info(
                 f"Chosen {chosen['name']} appeared in {chosen['count_of_issue_appearances']} comics"
             )
-            return chosen["api_detail_url"],
+            return (chosen["api_detail_url"],)
         else:
             raise ValueError(f"No character url found for {query}.")
 
@@ -181,10 +196,13 @@ class ComicVine:
         results = data.get("results", {})
         if results:
             chosen = self.random.choice(results["issue_credits"])
+            num_issue_credits = len(results["issue_credits"])
             api_detail_url = chosen["api_detail_url"]
-            self.call_map.append((self.get_random_character_appearance_url, len(results), args))
-            logging.info(f"Chose issue {chosen['site_detail_url']}")
-            return api_detail_url,
+            self.call_map.append(
+                (self.get_random_character_appearance_url, num_issue_credits, args)
+            )
+            logging.info(f"Chose issue {chosen['site_detail_url']} out of {num_issue_credits} appearances")
+            return (api_detail_url,)
         else:
             raise ValueError(f"No character url found for {query}.")
 
@@ -199,12 +217,13 @@ class ComicVine:
         results = data.get("results", [])
         if results:
             issue = self.random.choice(results["issues"])
+            num_issues = len(results['issues'])
             api_detail_url = issue["api_detail_url"]
-            self.call_map.append((self.get_random_comic_url, len(results), args))
+            self.call_map.append((self.get_random_comic_url, num_issues, args))
             logging.info(
-                f"Chose issue {issue['issue_number']} out of {len(results['issues'])}"
+                f"Chose issue {issue['issue_number']} out of {num_issues}"
             )
-            return api_detail_url,
+            return (api_detail_url,)
         else:
             raise ValueError("No comic issues found for the specified series.")
 
@@ -239,13 +258,18 @@ class ComicVine:
 
             logging.debug(f"Decision tree {self.call_map}")
 
-            issue_hash = self.get_hash(image_link)
-            if issue_hash in self.seen:
-                self.bummer = (image_link, image_name)
-                raise ComicCollisionError(f"Comic collision on {image_link}")
-            else:
-                self.add_hash(issue_hash)
+            if self.dedupe is True:
+                issue_hash = self.get_hash(image_link)
+                if issue_hash in self.seen:
+                    self.bummer = (image_link, image_name)
+                    if self.last_chance is True:
+                        logging.info("Dedupe comic: Failure - returning original")
+                    raise ComicCollisionError(f"Comic collision on {image_link}")
+                else:
+                    self.add_hash(issue_hash)
 
+            if self.last_chance is True:
+                logging.info("Dedupe comic: Success")
             return image_link, image_name
         else:
             raise ValueError("No comic issues found for the specified series.")
